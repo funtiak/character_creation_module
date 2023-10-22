19ecf769172e3afcad5d3a42fdaada5d48283ae4
Степан Корнеев <frick-ff@bk.ru>
2023-10-22 20:17:09 +0300

    Home edit 2

--- main.py
+++ main.py
@@ -90,6 +90,7 @@ def choice_char_class() -> str:
 
 
 def main() -> str:
+    """Главаная функция."""
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
@@ -100,6 +101,3 @@ def main() -> str:
     char_class: str = choice_char_class()
     print(start_training(char_name, char_class))
     return 'ничего'
-
-
-main()
